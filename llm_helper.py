ac75988732a90e0645aebff76c33502edc70f728
Devanshvora03 <131452370+Devanshvora03@users.noreply.github.com>
2024-11-18 11:09:03 +0530

    small change

--- llm_helper.py
+++ llm_helper.py
@@ -7,5 +7,5 @@ llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b
 
 
 if __name__ == "__main__":
-    response = llm.invoke("Two most important ingradient in samosa are ")
+    response = llm.invoke("What is the most important thing in universe to sruvive")
     print(response.content)
